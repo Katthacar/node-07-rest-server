c68ba09ffdf9894afed22848583c5d99cfe10675
Katthacar <hadercarrillo@gmail.com>
2019-07-09 15:37:59 -0500

    Enviroment Variable MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
  */
 const URL_DB = process.env.NODE_ENV === 'dev' ?
   'mongodb://localhost:27017/cafe' :
-  'mongodb+srv://Katthacar:Zywpn484TGg8w9z2@cluster0-5dxxg.mongodb.net/cafe'
+  process.env.MONGO_URI
 
 process.env.URL_DB = URL_DB;
